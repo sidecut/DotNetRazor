055304eab217279509fefe8689cd203798a51e2e
Jim Raden <jraden@streamlinksoftware.com>
2017-09-13 17:16:11 -0400

    Add Test Download page to simulate export

---
+++ Pages/TestDownload.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DotNetRazor.Pages
+{
+    public class TestDownloadModel : PageModel
+    {
+        [BindProperty]
+        public int SleepTime { get; set; } = 5000;
+
+        public void OnGet()
+        {
+
+        }
+
+        //public IActionResult OnPost()
+        //{
+        //    Debugger.Break();
+
+        //    return Page();
+        //}
+
+        public IActionResult OnPost()
+        {
+            System.Threading.Thread.Sleep(SleepTime);
+
+            Response.Headers["Content-disposition"] = "Attachment; filename=\"Flintstones.csv\"";
+            return new ContentResult()
+            {
+                Content = "FirstName,LastName,Age\nFred,Flintstone,35\nBarney,Rubble,35\nWilma,Flintstone,33\nBetty,Rubble,33",
+                ContentType = "text/csv"
+            };
+        }
+    }
+}
